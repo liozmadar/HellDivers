0c90cc6f23c25df3fb0d924ef0642a4579b5f742
liozmadar <liozmadar@gmail.com>
2022-04-11 17:59:13 +0300

    monster health bar canvas
    bar looking at the camera

--- Assets/Scripts/Enemy/FloatingText.cs
+++ Assets/Scripts/Enemy/FloatingText.cs
@@ -5,20 +5,23 @@ using TMPro;
 
 public class FloatingText : MonoBehaviour
 {
+    public Camera myCamera;
+
     public float floatingTextDestroyTimer = 2f;
     public Vector3 offSet = new Vector3(0, 4, 0);
     public Vector3 randomTextPosition = new Vector3(1f, 0, 0);
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, floatingTextDestroyTimer);
-
+        // Destroy(gameObject, floatingTextDestroyTimer);
 
         transform.localPosition += offSet;
 
         transform.localPosition += new Vector3(Random.Range(-randomTextPosition.x, randomTextPosition.x),
             Random.Range(-randomTextPosition.y, randomTextPosition.y),
             Random.Range(-randomTextPosition.z, randomTextPosition.z));
+
+        transform.LookAt(transform.position + myCamera.transform.rotation * Vector3.back, myCamera.transform.rotation * Vector3.up);
     }
 
     // Update is called once per frame
