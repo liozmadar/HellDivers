4d5705cd5b62022d441c9865ed17bb7047e02f78
liozmadar <liozmadar@gmail.com>
2022-04-13 11:29:25 +0300

    spaceShipSphere Timer

--- Assets/Scripts/AirCraft/TakeThePlayerUp.cs
+++ Assets/Scripts/AirCraft/TakeThePlayerUp.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class TakeThePlayerUp : MonoBehaviour
 {
+    public GameObject spaceShip;
     public GameObject player;
+    public float shipSphereTimer = 10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,13 +16,23 @@ public class TakeThePlayerUp : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ShipSphereTimer();
+
+
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             player.transform.position = transform.position;
+        }
+    }
+    void ShipSphereTimer()
+    {
+        shipSphereTimer -= Time.deltaTime;
+        if (shipSphereTimer <= 0)
+        {
+            transform.position = new Vector3(spaceShip.transform.position.x, spaceShip.transform.position.y - 1, spaceShip.transform.position.z);
         }
     }
     void GetIntoTheShip()
