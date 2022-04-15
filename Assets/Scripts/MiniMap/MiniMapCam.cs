c18274857a65c6b28aa811dc5ed06c9e77f62020
liozmadar <liozmadar@gmail.com>
2022-04-15 14:03:45 +0300

    MiniMap More fixing

--- Assets/Scripts/MiniMap/MiniMapCam.cs
+++ Assets/Scripts/MiniMap/MiniMapCam.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class MiniMapCam : MonoBehaviour
 {
     public GameObject player;
+    public float miniMapCamDistance = 100f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +16,6 @@ public class MiniMapCam : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 100, player.transform.position.z);
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + miniMapCamDistance, player.transform.position.z);
     }
 }
