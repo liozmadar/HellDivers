4155bc64a037e32a81297785a2a0458e5793c492
liozmadar <liozmadar@gmail.com>
2022-04-19 14:55:38 +0300

    Revert "DropLoot is a little faster"
    This reverts commit a4858a10455bb031b86e61202a7a2a6574920df5.

--- Assets/Scripts/DropLoot/DropLootFollow.cs
+++ Assets/Scripts/DropLoot/DropLootFollow.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class DropLootFollow : MonoBehaviour
 {
     public GameObject player;
-    public float minModifier = 20f;
-    public float maxModifier = 30f;  
+    public float minModifier = 1f;
+    public float maxModifier = 2f;  
 
     private bool isFollowing;
 
