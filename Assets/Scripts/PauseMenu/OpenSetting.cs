c847bbebb6c9be9bfdb039cab2c9c692f7e6c80f
liozmadar <liozmadar@gmail.com>
2022-04-20 15:56:24 +0300

    MenuSettings added (still has work to do)

---
+++ Assets/Scripts/PauseMenu/OpenSetting.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenSetting : MonoBehaviour
+{
+    public GameObject settingsImage;
+    private bool settigsOpenClose = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OpenSettings()
+    {
+        if (settigsOpenClose)
+        {
+            settingsImage.SetActive(true);
+            settigsOpenClose = false;
+        }
+        else if (!settigsOpenClose)
+        {
+            CloseSettings();
+            settigsOpenClose = true;
+        }
+    }
+    void CloseSettings()
+    {
+        settingsImage.SetActive(false);
+    }
+}
