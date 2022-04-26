535c118a2acdfeeeb020bfe9b29e7a0742f06700
liozmadar <liozmadar@gmail.com>
2022-04-26 14:15:27 +0300

    Enemies spawn speed is faster now

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -66,11 +66,11 @@ public class EnemySpawner : MonoBehaviour
     {
         if (SpawnSpeedTimer > 30 && SpawnSpeedTimer < 60)
         {
-            reSpawnTimer = 7;
+            reSpawnTimer = 5;   
         }
         else if (SpawnSpeedTimer > 60 && SpawnSpeedTimer < 90)
         {
-            reSpawnTimer = 5;
+            reSpawnTimer = 4;
         }
         else if (SpawnSpeedTimer > 90)
         {
